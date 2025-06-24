ee72cf4a0c399abed1e6c8a0464a0aa7cc3e7c46
harfool <harfoolgujjar63@gmail.com>
2025-06-24 11:07:11 +0530

    prisma user model and install

---
+++ backend/Auth backend SQL/prisma/migrations/20250624052738_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "verificationToken" TEXT NOT NULL,
+    "passwordResetToken" TEXT NOT NULL,
+    "passwordResetExpires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
